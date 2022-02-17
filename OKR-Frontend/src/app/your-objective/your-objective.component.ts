b85c947ca4da23f02a1a18d3278516c31868e07e
Aditya Raj <ADITYARAJ61522@GMAIL.COM>
2022-02-17 16:25:51 +0530

    create objevtive form object forming

--- OKR-Frontend/src/app/your-objective/your-objective.component.ts
+++ OKR-Frontend/src/app/your-objective/your-objective.component.ts
@@ -1,5 +1,6 @@
 import { tokenize } from '@angular/compiler/src/ml_parser/lexer';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-your-objective',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./your-objective.component.css']
 })
 export class YourObjectiveComponent implements OnInit {
+  constructor() { }
 
   show=false;
   show2=false;
+
+  newObjective={
+    goal_name: "",
+    goal_description: "",
+    goal_start_date: "",
+    goal_due_date: "",
+    goal_quater: "",
+    goal_owner_name: "",
+    linked_org_goal_id: ""
+  }
+
+  @ViewChild('ObjForm') ObjFormData!: NgForm;
+  addObjective(){
+    this.newObjective.goal_name=this.ObjFormData.value.goal_name;
+    this.newObjective.goal_description=this.ObjFormData.value.goal_description;
+    this.newObjective.goal_start_date=this.ObjFormData.value.goal_start_date;
+    this.newObjective.goal_due_date=this.ObjFormData.value.goal_due_date;
+    this.newObjective.goal_quater=this.ObjFormData.value.goal_quater;
+    this.newObjective.goal_owner_name=this.ObjFormData.value.goal_owner_name;
+    this.newObjective.linked_org_goal_id=this.ObjFormData.value.linked_org_goal_id;
+    // this.show=false;
+    // this.show2=true;
+
+    console.log(this.newObjective)
+  }
   objectiveShow(){
     this.show=true;
   }
@@ -18,10 +45,6 @@ export class YourObjectiveComponent implements OnInit {
     this.show=false;
   }
 
-  keyresultShow(){
-    this.show=false;
-    this.show2=true;
-  }
   keyresultHide(){
     this.show2=false;
   }
@@ -29,7 +52,7 @@ export class YourObjectiveComponent implements OnInit {
     this.show2=false;
     this.show=true;
   }
-  constructor() { }
+
 
   userdata={
     token:"",
