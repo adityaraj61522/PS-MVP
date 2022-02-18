03e03bd60b1813d55ef4b49ed85ee72861e30a3a
Jahiz Habibullah <jahiz446@gmail.com>
2022-02-18 16:15:18 +0530

    register component

--- OKR-Frontend/src/app/register/register.component.ts
+++ OKR-Frontend/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import {FormGroup,FormBuilder, Validators} from '@angular/forms'
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -10,34 +11,61 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  registerForm!: FormGroup;
-  registerObj: any;
-  
-  constructor(private fb :FormBuilder, private http : HttpClient,private router : Router) { }
+  userData={
+    firstname:"",
+    lastname:"",
+    email:"",
+    mobile:"",
+    line_manager_id:"",
+    emp_code:"",
+    org_id:"",
+    homeTown:"",
+    gender:"",
+    emp_dept:"",
+    password:"",
+  }
+  userExist=false;
 
-  ngOnInit(): void {
-    this.registerForm = this.fb.group({
-      firstname:["", Validators.required],
-      lastname:["", Validators.required],
-      mobile:["",Validators.required],
-      username:["",Validators.compose([Validators.required,Validators.email])],
-      password:["",Validators.required],
-      emp_id:["",Validators.required],
-      city:["",Validators.required],
-      country:["",Validators.required]
-    })
+  @ViewChild('f') formData!: NgForm;
+  registerForm!:  FormGroup;
+
+  constructor( private http : HttpClient, private router : Router) { }
+
+  submit(){
+    this.userData.firstname=this.formData.value.firstname;
+    this.userData.lastname=this.formData.value.lastname;
+    this.userData.email=this.formData.value.email;
+    this.userData.mobile=this.formData.value.mobile;
+    this.userData.line_manager_id=this.formData.value.l1_manager;
+    this.userData.emp_code=this.formData.value.emp_code;
+    this.userData.org_id=this.formData.value.org_id;
+    this.userData.gender=this.formData.value.gender;
+    this.userData.homeTown=this.formData.value.homeTown;
+    this.userData.password=this.formData.value.password;
+    this.userData.emp_dept=this.formData.value.emp_dept;
+    console.log(this.userData);
+
+  
   }
 
-//   register(){
+  ngOnInit(): void {
+  }
 
-//   this.registerObj.FirstName = this.registerForm.value.firstname;
-//   this.registerObj.LastName = this.registerForm.value.lastname;
-//   this.registerObj.UserName = this.registerForm.value.username;
-//   this.registerObj.Password = this.registerForm.value.password;
-//   this.registerObj.Emp_Id = this.registerForm.value.emp_id;
-//   this.registerObj.Mobile = this.registerForm.value.mobile;
-//   this.registerObj.city = this.registerForm.value.city;
-//   this.registerObj.country = this.registerForm.value.country;
-  
-// }
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      firstname: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      lastname: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      mobile: new FormControl("", [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$"), Validators.minLength(10)]),
+      line_manager_id: new FormControl("", [Validators.required, Validators.minLength(2)]),
+      emp_code: new FormControl("", [Validators.required]),
+      org_id: new FormControl("", [Validators.required]),
+      gender: new FormControl("", [Validators.required]),
+      homeTown: new FormControl("", [Validators.required]),
+      password: new FormControl("", [Validators.required, Validators.minLength(6)]),
+      emp_dept: new FormControl("",[Validators.required]),
+    });
+  }
+ 
+
 }
