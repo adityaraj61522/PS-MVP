a2c32d9a4106e22d95100d068275e6ff18050545
Aditya Raj <ADITYARAJ61522@GMAIL.COM>
2022-02-22 08:17:14 +0530

    form

--- OKR-Frontend/src/app/your-objective/your-objective.component.ts
+++ OKR-Frontend/src/app/your-objective/your-objective.component.ts
@@ -1,6 +1,8 @@
+import { HttpClientModule } from '@angular/common/http';
 import { tokenize } from '@angular/compiler/src/ml_parser/lexer';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-your-objective',
@@ -8,7 +10,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./your-objective.component.css']
 })
 export class YourObjectiveComponent implements OnInit {
-  constructor() { }
+  constructor( private http : HttpClient) { }
 
   show=false;
   show2=false;
@@ -18,24 +20,53 @@ export class YourObjectiveComponent implements OnInit {
     goal_description: "",
     goal_start_date: "",
     goal_due_date: "",
-    goal_quater: "",
     goal_owner_name: "",
     linked_org_goal_id: ""
   }
 
+  newKey={
+    milestone_name:"",
+    milestone_type:"",
+    milestone_start_date:"",
+    milestone_due_date:""
+  }
+
   @ViewChild('ObjForm') ObjFormData!: NgForm;
+  @ViewChild('KeyForm') KeyResFormData!: NgForm;
   addObjective(){
     this.newObjective.goal_name=this.ObjFormData.value.goal_name;
     this.newObjective.goal_description=this.ObjFormData.value.goal_description;
     this.newObjective.goal_start_date=this.ObjFormData.value.goal_start_date;
     this.newObjective.goal_due_date=this.ObjFormData.value.goal_due_date;
-    this.newObjective.goal_quater=this.ObjFormData.value.goal_quater;
     this.newObjective.goal_owner_name=this.ObjFormData.value.goal_owner_name;
     this.newObjective.linked_org_goal_id=this.ObjFormData.value.linked_org_goal_id;
     // this.show=false;
     // this.show2=true;
+    
+    this.http.post(`http://localhost:9001/api/v1/employee/test`,this.newObjective).subscribe((result)=>{
+      console.log(result); 
+      // this.responseData=JSON.parse(JSON.stringify(result));
+      // sessionStorage.setItem("token" , this.responseData.token);
+      // sessionStorage.setItem("expires" , this.responseData.expires);
+      // sessionStorage.setItem("userData" , this.responseData.user);
+      // this.correctPassword=true;
+      // this.router.navigate(
+      //   ['/objectives']
+      // );
+    },(error)=>{
+      console.error(error);
+      // this.correctPassword=false;
+    });
 
     console.log(this.newObjective)
+  }
+
+  addKey(){
+    this.newKey.milestone_name=this.KeyResFormData.value.milestone_name;
+    this.newKey.milestone_type=this.KeyResFormData.value.milestone_type;
+    this.newKey.milestone_start_date=this.KeyResFormData.value.milestone_start_date;
+    this.newKey.milestone_due_date=this.KeyResFormData.value.milestone_due_date;
+    console.log(this.newKey);
   }
   objectiveShow(){
     this.show=true;
