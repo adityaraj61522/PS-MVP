b55547363d9a1b3d8b09594065acd590dc2df3b6
Jahiz Habibullah <jahiz446@gmail.com>
2022-02-22 12:50:02 +0530

    register component

--- OKR-Frontend/src/app/register/register.component.ts
+++ OKR-Frontend/src/app/register/register.component.ts
@@ -11,18 +11,18 @@ import { HttpClient } from '@angular/common/http';
 export class RegisterComponent implements OnInit {
 
   userData={
-    firstname:"",
-    lastname:"",
+    first_name:"",
+    last_name:"",
     username:"",
     email:"",
-    mobile:"",
+    //mobile:"",
     emp_code:"",
     org_id:"",
     line_manager_id:"",
     city:"",
     country:"",
-    gender:"",
-    emp_dept:"",
+    //gender:"",
+    department:"",
     password:"",
   }
   userExist=false;
@@ -32,24 +32,24 @@ export class RegisterComponent implements OnInit {
   @ViewChild('f') formData!:NgForm;
 
   submit(){
-    this.userData.firstname=this.formData.value.firstname;
-    this.userData.lastname=this.formData.value.lastname;
+    this.userData.first_name=this.formData.value.first_name;
+    this.userData.last_name=this.formData.value.last_name;
     this.userData.username=this.formData.value.username;
     this.userData.email=this.formData.value.email;
-    this.userData.mobile=this.formData.value.mobile;
+    //this.userData.mobile=this.formData.value.mobile;
     this.userData.emp_code=this.formData.value.emp_code;
     this.userData.org_id=this.formData.value.org_id;
     this.userData.line_manager_id=this.formData.value.line_manager_id;
-    this.userData.gender=this.formData.value.gender;
+    //this.userData.gender=this.formData.value.gender;
     this.userData.city=this.formData.value.city;
     this.userData.country=this.formData.value.country;
     this.userData.password=this.formData.value.password;
-    this.userData.emp_dept=this.formData.value.emp_dept;
+    this.userData.department=this.formData.value.department;
     console.log(this.userData);
 
-    this.http.post('/register-user',this.userData).subscribe((result)=>{
-      console.warn(result); 
-      this.router.navigate(['/login'])
+    this.http.post(`http://localhost:9001/register-user`,this.userData).subscribe((result)=>{
+      console.log(result); 
+      //this.router.navigate([`./login`])
   },(error)=>{
     console.error(error);
     this.userExist=true;
