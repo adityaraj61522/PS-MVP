a8d8ae2b259638736bc2830e38e1bd8349f42b13
Aditya Raj <ADITYARAJ61522@GMAIL.COM>
2022-02-23 13:25:50 +0530

    ommit

--- OKR-Frontend/src/app/app.module.ts
+++ OKR-Frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { YourObjectiveComponent } from './your-objective/your-objective.componen
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { ObjectiveDetailsComponent } from './objective-details/objective-details.component';
+import { GoalListComponent } from './your-objective/goal-list/goal-list.component';
 
 @NgModule({
   declarations: [
@@ -19,13 +21,15 @@ import { ObjectiveDetailsComponent } from './objective-details/objective-details
     LoginComponent,
     RegisterComponent,
     ObjectiveDetailsComponent,
+    GoalListComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule,
+    HttpClientModule, 
+    // NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ OKR-Frontend/src/app/your-objective/goal-list/goal-list.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-goal-list',
+  templateUrl: './goal-list.component.html',
+  styleUrls: ['./goal-list.component.css']
+})
+export class GoalListComponent implements OnInit {
+
+  constructor() { }
+
+  show3=false;
+  dp=false;
+  showDp(){
+    if(this.dp == false){
+      this.dp=true;
+    }else{
+      this.dp =false;
+    }
+  }
+  openUpdateDp(){
+    this.show3=true;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
