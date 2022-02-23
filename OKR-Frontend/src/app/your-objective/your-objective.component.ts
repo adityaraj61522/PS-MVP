90c366c754e88f2b867552c0b15291bef526b5cc
AnkitKuamr117 <ankitpandel1234@gmail.com>
2022-02-23 18:39:02 +0530

    .

--- OKR-Frontend/src/app/your-objective/your-objective.component.ts
+++ OKR-Frontend/src/app/your-objective/your-objective.component.ts
@@ -181,6 +181,7 @@ export class YourObjectiveComponent implements OnInit {
     this.http.post(`/api/v1/employee/getgoals`, this.getGoal, requestOptions).subscribe((response)=>{
       console.log(response);
       this.goalData=response;
+      console.log("goal_DATA:---", this.goalData)
       
     },(error)=>{
       console.error(error);
