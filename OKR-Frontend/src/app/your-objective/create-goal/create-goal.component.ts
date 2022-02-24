4c0378dd4ddb0c5db3091a0556cbc7fb0c224215
Aditya Raj <ADITYARAJ61522@GMAIL.COM>
2022-02-24 12:08:14 +0530

    typeahead

--- OKR-Frontend/src/app/your-objective/create-goal/create-goal.component.ts
+++ OKR-Frontend/src/app/your-objective/create-goal/create-goal.component.ts
@@ -25,7 +25,7 @@ export class CreateGoalComponent implements OnInit {
     goal_owner_name: "",
     linked_org_goal_id: "",
     org_id:"",
-    goal_owner_id:1,
+    goal_owner_id:"",
     goal_owner_email:"",
     goal_type:"",
     goal_setting_id:1,
@@ -49,8 +49,6 @@ export class CreateGoalComponent implements OnInit {
     this.userData=this.user[0]
     // console.log(this.userData.first_name);
     this.newObjective.org_id=this.userData.org_id;
-    // this.newObjective.goal_owner_id=this.userData.first_name;
-    // this.newObjective.goal_owner_email=this.userData.first_name;
     // this.newObjective.goal_setting_id=this.userData.first_name;
     this.newObjective.created_by=this.userData.user_id;
     // this.newObjective.updated_by=this.userData.first_name;
@@ -73,11 +71,15 @@ export class CreateGoalComponent implements OnInit {
      await this.http.post(`/api/v1/employee/getusers`, this.getUser, requestOptions).subscribe((response)=>{
       console.log(response,"user");
       this.allUsers=response;
-      console.log(this.allUsers,"al")
+      console.log(this.allUsers[0],"al")
     },(error)=>{
       console.error(error);
     })
+
   }
+
+inFormatter = (x: {full_name: string}) => x.full_name;
+outFormatter = (x: {full_name: string}) => x.full_name;
 
   d=["abc","abcc","abcd","abcde","abcssss","abcddd"]
 
@@ -87,15 +89,18 @@ export class CreateGoalComponent implements OnInit {
       debounceTime(200),
       distinctUntilChanged(),
       map(term => term.length < 1 ? []
-        : this.allUsers.filter((v: any) => v.full_name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+        : this.allUsers.filter((v: any) => v.full_name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)),
     )
+    
 
   addObjective(){
     this.newObjective.goal_name=this.ObjFormData.value.goal_name;
     this.newObjective.goal_type=this.ObjFormData.value.goal_type;
     this.newObjective.goal_start_date=this.ObjFormData.value.goal_start_date;
     this.newObjective.goal_due_date=this.ObjFormData.value.goal_due_date;
-    this.newObjective.goal_owner_name=this.ObjFormData.value.goal_owner_name;
+    this.newObjective.goal_owner_name=this.model.full_name;
+    this.newObjective.goal_owner_id=this.model.user_id;
+    this.newObjective.goal_owner_email=this.model.email;
     this.newObjective.linked_org_goal_id=this.ObjFormData.value.linked_org_goal_id;
     // this.show=false;
     // this.show2=true;
