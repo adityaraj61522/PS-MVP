ccc332fe026903a9f32b7e1b855043592a5f2c82
RAVI KUMAR <ravikumar444411@gmail.com>
2022-02-24 12:39:40 +0530

    fix issue

--- OKR-Frontend/src/app/milestone/milestone.component.ts
+++ OKR-Frontend/src/app/milestone/milestone.component.ts
@@ -3,6 +3,17 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import {  NgForm } from '@angular/forms';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ApiService } from '../apiCollection/api.service';
+import {Observable, OperatorFunction} from 'rxjs';
+import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
 
 @Component({
   selector: 'app-milestone',
@@ -25,6 +36,45 @@ export class MilestoneComponent implements OnInit {
   show=false;
   show2=false;
   choice:string = 'boolean';
+  
+  // test now start
+  public model: any;
+
+  demo:any = [
+    {id:1,name:'ravi',text:'hello 1'},
+    {id:2,name:'ravi 2',text:'hello 2'},
+    {id:3,name:'ravi 3',text:'hello 3'}
+  ]
+
+     search: OperatorFunction<string, readonly string[]> = (text$: Observable<string>) =>
+        text$.pipe(
+          debounceTime(200),
+          distinctUntilChanged(),
+          map(term =>   term.length < 2 ? []
+            : this.demo.filter((v: any)=> {
+              // console.log(v.name);
+              // console.log(term);
+              if( !v.name.indexOf(term.toLocaleLowerCase())    ){
+                console.log(v.name);
+                
+                this.model = v.name
+                return v
+              }
+
+
+            //   let obj = this.demo.find((o:any) => o.name === term);
+
+            // console.log(obj);
+
+
+
+            // return  v.indexOf(term.toLowerCase()) > -1
+            }))
+          )
+
+
+  //test now end
+  
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
@@ -117,5 +167,9 @@ export class MilestoneComponent implements OnInit {
     
   }
 
+}
+
+function v(v: any, arg1: (any: any) => boolean): any {
+  throw new Error('Function not implemented.');
 }
  
