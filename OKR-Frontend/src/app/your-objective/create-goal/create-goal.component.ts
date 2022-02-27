5096889ac1351dc50c202acbab228ba3a73b0f04
Aditya Raj <ADITYARAJ61522@GMAIL.COM>
2022-02-28 00:06:30 +0530

    fix

--- OKR-Frontend/src/app/your-objective/create-goal/create-goal.component.ts
+++ OKR-Frontend/src/app/your-objective/create-goal/create-goal.component.ts
@@ -1,5 +1,5 @@
 import { HttpClientModule, HttpEvent, HttpHandler, HttpHeaders, HttpRequest } from '@angular/common/http';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild,Input } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { NgStyle } from '@angular/common';
@@ -13,139 +13,8 @@ import { debounceTime,switchMap, distinctUntilChanged, map } from 'rxjs/operator
   styleUrls: ['./create-goal.component.css']
 })
 export class CreateGoalComponent implements OnInit {
-  model: any;
-  modelOrgGoal:any;
-
-  constructor( private http : HttpClient ) { }
-
-  newObjective={
-    goal_name: "",
-    goal_description: "",
-    goal_start_date: "",
-    goal_due_date: "",
-    goal_owner_name: "",
-    linked_org_goal_id: "",
-    org_id:"",
-    goal_owner_id:"",
-    goal_owner_email:"",
-    // goal_type:"",
-    goal_setting_id:1,
-    created_by:'',
-    updated_by:1
-  }
-
-  @ViewChild('ObjForm') ObjFormData!: NgForm;
-
-  user="";
-  userData:any;
-  
-  getUser={
-    org_id:""
-  }
-  allUsers:any;
-  allOrgGoal:any;
-
-  async ngOnInit(): Promise<void> {
-    this.user=JSON.parse(JSON.parse(JSON.stringify(sessionStorage.getItem("userData"))));
-    // console.log(this.userdata.user[0],"kjhskjkj");
-    this.userData=this.user[0]
-    this.newObjective.org_id=this.userData.org_id;
-    // this.newObjective.goal_setting_id=this.userData.first_name;
-    this.newObjective.created_by=this.userData.user_id;
-    // this.newObjective.updated_by=this.userData.first_name;
-    this.getUser.org_id=this.userData.org_id;
-    console.log(this.getUser, "jhvjhcds")
-
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Accept': 'application/json',
-      'Access-Control-Allow-Headers': '*',
-      'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
-      'x-key':'1',
-      'x-org':'1'
-    }
-    const requestOptions = {
-      headers: new HttpHeaders(headers),
-    };
-
-    // Get Users
-     await this.http.post(`/api/v1/employee/getusers`, this.getUser, requestOptions).subscribe((response)=>{
-      console.log(response,"user");
-      this.allUsers=response;
-      console.log(this.allUsers[0],"al")
-    },(error)=>{
-      console.error(error);
-    })
-
-    // Get Orginizational goal
-    await this.http.post(`/api/v1/employee/getorganizationgoals`, this.getUser, requestOptions).subscribe((response)=>{
-      console.log(response,"user");
-      this.allOrgGoal=response;
-      console.log(this.allOrgGoal,"or")
-    },(error)=>{
-      console.error(error);
-    })
-
-  }
-
-inFormatter = (x: {goal_name: string}) => x.goal_name;
-outFormatter = (x: {full_name: string}) => x.full_name;
-
-  d=["abc","abcc","abcd","abcde","abcssss","abcddd"]
-
-  search: OperatorFunction<string, readonly string[]> = (text$: 
-    Observable<string>) =>
-    text$.pipe(
-      debounceTime(200),
-      distinctUntilChanged(),
-      map(term => term.length < 1 ? []
-        : this.allUsers.filter((v: any) => v.full_name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)),
-    )
-
-  searchOrgGoal: OperatorFunction<string, readonly string[]> = (text$: 
-      Observable<string>) =>
-      text$.pipe(
-        debounceTime(200),
-        distinctUntilChanged(),
-        map(term => term.length < 1 ? []
-          : this.allOrgGoal.filter((v: any) => v.goal_name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)),
-      )
-
-
-    
-
-  addObjective(){
-    this.newObjective.goal_name=this.ObjFormData.value.goal_name;
-    // this.newObjective.goal_type=this.ObjFormData.value.goal_type;
-    this.newObjective.goal_start_date=this.ObjFormData.value.goal_start_date;
-    this.newObjective.goal_due_date=this.ObjFormData.value.goal_due_date;
-    this.newObjective.goal_owner_name=this.model.full_name;
-    this.newObjective.goal_owner_id=this.model.user_id;
-    this.newObjective.goal_owner_email=this.model.email;
-    this.newObjective.linked_org_goal_id=this.ObjFormData.value.linked_org_goal_id;
-    // this.show=false;
-    // this.show2=true;
-    const headers = {
-    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Accept': 'application/json',
-    'Access-Control-Allow-Headers': '*',
-    'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
-    'x-key':'1',
-    'x-org':'1'
-  }  
-  const requestOptions = {
-    headers: new HttpHeaders(headers),
-  };
-    
-    this.http.post(`/api/v1/employee/create-objective`, this.newObjective , requestOptions
-  ).subscribe((result)=>{
-      console.log(result); 
-      // this.show=false;
-      // this.show2=true;
-    },(error)=>{
-      console.error(error);
-    });
-    console.log(this.newObjective ,"obj")
+  ngOnInit(): void {
+      
   }
 
 
--- OKR-Frontend/src/app/your-objective/your-objective.component.ts
+++ OKR-Frontend/src/app/your-objective/your-objective.component.ts
@@ -14,18 +14,17 @@ import { debounceTime,switchMap, distinctUntilChanged, map } from 'rxjs/operator
   styleUrls: ['./your-objective.component.css']
 })
 export class YourObjectiveComponent implements OnInit {
-
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
-    const headers = new HttpHeaders({
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
-      'x-key':'1',
-      'x-org':'1'
-    });
-    const cloneReq = req.clone({headers});
-    return next.handle(cloneReq);
-  }
+  headers = {
+    'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+    'Accept': 'application/json',
+    'Access-Control-Allow-Headers': '*',
+    'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
+    'x-key':'1',
+    'x-org':'1'
+  }  
+  requestOptions = {
+    headers: new HttpHeaders(this.headers),
+  };
   constructor( private http : HttpClient) { }
   show=false;
   show2=false;
@@ -76,22 +75,11 @@ export class YourObjectiveComponent implements OnInit {
   }
 
   deleteGoal=(goal_name: any, goal_id:any , org_id: any)=>{
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Accept': 'application/json',
-      'Access-Control-Allow-Headers': '*',
-      'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
-      'x-key':'1',
-      'x-org':'1'
-    }
-    const requestOptions = {
-      headers: new HttpHeaders(headers),
-    };
     this.deleteGoalReq.goal_id= goal_id;
     this.deleteGoalReq.org_id = org_id;
     console.log(this.deleteGoalReq)
     if(confirm("Are you sure want to delete "+goal_name)){
-      this.http.put(`/api/v1/employee/deletegoal`, {goal_id,org_id} , requestOptions).subscribe((response)=>{
+      this.http.put(`/api/v1/employee/deletegoal`, {goal_id,org_id} , this.requestOptions).subscribe((response)=>{
         console.log("delete goal console:---", response);
         window.location.reload();
       },(error)=>{
@@ -99,26 +87,9 @@ export class YourObjectiveComponent implements OnInit {
       })
     }
   }
-
-
-
-
   userData:any;
-
   allUsers:any;
   goalData: any;
-
-  // public deleteGoal=(goal_name: any, goal_id:any , org_id: any)=>{
-  //   if(confirm("Are you sure want to delete "+goal_name)){
-  //     this.http.post(`/api/v1/employee/getgoals`, this.getGoal).subscribe((response)=>{
-  //       // console.log(response);
-  //       this.goalData=response;      
-  //       console.log("goal_DATA:---", this.goalData)   
-  //     },(error)=>{
-  //       console.error(error);
-  //     })
-  //   }
-  // }
   async ngOnInit(): Promise<void> {
     this.userdata.token=JSON.parse(JSON.stringify(sessionStorage.getItem("token")));
     this.userdata.expires=JSON.parse(JSON.stringify(sessionStorage.getItem("expires")));
@@ -131,21 +102,8 @@ export class YourObjectiveComponent implements OnInit {
 
 
     this.getUser.org_id=this.userData.org_id;
-    const headers = {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Accept': 'application/json',
-      'Access-Control-Allow-Headers': '*',
-      'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
-      'x-key':'1',
-      'x-org':'1'
-    }
-    const requestOptions = {
-      headers: new HttpHeaders(headers),
-    };
-    
-
     // Get Goals
-    this.http.post(`/api/v1/employee/getgoals`, this.getGoal, requestOptions).subscribe((response)=>{
+    this.http.post(`/api/v1/employee/getgoals`, this.getGoal, this.requestOptions).subscribe((response)=>{
       // console.log(response);
       this.goalData=response;
       
@@ -164,9 +122,110 @@ export class YourObjectiveComponent implements OnInit {
     // })
 
     // console.log(this.allUsers,"all")
+
+    // Create-goal content
+    this.user=JSON.parse(JSON.parse(JSON.stringify(sessionStorage.getItem("userData"))));
+    // console.log(this.userdata.user[0],"kjhskjkj");
+    this.userData=this.user[0]
+    this.newObjective.org_id=this.userData.org_id;
+    // this.newObjective.goal_setting_id=this.userData.first_name;
+    this.newObjective.created_by=this.userData.user_id;
+    // this.newObjective.updated_by=this.userData.first_name;
+    this.getUser.org_id=this.userData.org_id;
+    console.log(this.getUser, "jhvjhcds")
+
+    // Get Users
+     await this.http.post(`/api/v1/employee/getusers`, this.getUser, this.requestOptions).subscribe((response)=>{
+      console.log(response,"user");
+      this.allUsers=response;
+      console.log(this.allUsers[0],"al")
+    },(error)=>{
+      console.error(error);
+    })
+
+    // Get Orginizational goal
+    await this.http.post(`/api/v1/employee/getorganizationgoals`, this.getUser, this.requestOptions).subscribe((response)=>{
+      console.log(response,"user");
+      this.allOrgGoal=response;
+      console.log(this.allOrgGoal,"or")
+    },(error)=>{
+      console.error(error);
+    })
   }
-  // updateObjective(){
-      
-  // }
+
+  // create Objective content
+
+  model: any;
+  modelOrgGoal:any;
+
+  newObjective={
+    goal_name: "",
+    goal_description: "",
+    goal_start_date: "",
+    goal_due_date: "",
+    goal_owner_name: "",
+    linked_org_goal_id: "",
+    org_id:"",
+    goal_owner_id:"",
+    goal_owner_email:"",
+    // goal_type:"",
+    goal_setting_id:1,
+    created_by:'',
+    updated_by:1
+  }
+
+  @ViewChild('ObjForm') ObjFormData!: NgForm;
+
+  user="";
+  allOrgGoal:any;
+
+inFormatter = (x: {goal_name: string}) => x.goal_name;
+outFormatter = (x: {full_name: string}) => x.full_name;
+
+  d=["abc","abcc","abcd","abcde","abcssss","abcddd"]
+
+  search: OperatorFunction<string, readonly string[]> = (text$: 
+    Observable<string>) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 1 ? []
+        : this.allUsers.filter((v: any) => v.full_name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)),
+    )
+
+  searchOrgGoal: OperatorFunction<string, readonly string[]> = (text$: 
+      Observable<string>) =>
+      text$.pipe(
+        debounceTime(200),
+        distinctUntilChanged(),
+        map(term => term.length < 1 ? []
+          : this.allOrgGoal.filter((v: any) => v.goal_name.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10)),
+      )
+
+
+    
+
+  addObjective(){
+    this.newObjective.goal_name=this.ObjFormData.value.goal_name;
+    // this.newObjective.goal_type=this.ObjFormData.value.goal_type;
+    this.newObjective.goal_start_date=this.ObjFormData.value.goal_start_date;
+    this.newObjective.goal_due_date=this.ObjFormData.value.goal_due_date;
+    this.newObjective.goal_owner_name=this.model.full_name;
+    this.newObjective.goal_owner_id=this.model.user_id;
+    this.newObjective.goal_owner_email=this.model.email;
+    this.newObjective.linked_org_goal_id=this.ObjFormData.value.linked_org_goal_id;
+    this.show=false;
+    this.show2=true;
+    
+    this.http.post(`/api/v1/employee/create-objective`, this.newObjective , this.requestOptions
+  ).subscribe((result)=>{
+      console.log(result); 
+      this.show=false;
+      this.show2=true;
+    },(error)=>{
+      console.error(error);
+    });
+    console.log(this.newObjective ,"obj")
+  }
   
 }
