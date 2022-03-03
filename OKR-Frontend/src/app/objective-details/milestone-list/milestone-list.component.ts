c73fbfe7168be3d3c1234a53495691130049bb42
AnkitKuamr117 <ankitpandel1234@gmail.com>
2022-03-03 12:21:59 +0530

    goal_id in delete goal

--- OKR-Frontend/src/app/objective-details/milestone-list/milestone-list.component.ts
+++ OKR-Frontend/src/app/objective-details/milestone-list/milestone-list.component.ts
@@ -3,32 +3,34 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-milestone-list',
   templateUrl: './milestone-list.component.html',
-  styleUrls: ['./milestone-list.component.css']
+  styleUrls: ['./milestone-list.component.css'],
 })
 export class MilestoneListComponent implements OnInit {
-
-  constructor() { }
-  @Input() public deleteMilestone !: (milestone_name: any, milestone_id:any , org_id: any) => void;
-  @Input() milestoneDetails:any;
-  dp=false;
-  check=false;
-  checkin(){
-    if(this.check==false){
-      this.check=true;
-    }else{
-      this.check=false;
+  constructor() {}
+  @Input() public deleteMilestone!: (
+    milestone_name: any,
+    milestone_id: any,
+    org_id: any,
+    goal_id: any
+  ) => void;
+  @Input() milestoneDetails: any;
+  dp = false;
+  check = false;
+  checkin() {
+    if (this.check == false) {
+      this.check = true;
+    } else {
+      this.check = false;
     }
-
   }
-  showDp(){
-    if(this.dp == false){
-      this.dp=true;
-    }else{
-      this.dp =false;
+  showDp() {
+    if (this.dp == false) {
+      this.dp = true;
+    } else {
+      this.dp = false;
     }
   }
   ngOnInit(): void {
-    console.log(this.milestoneDetails,"hjdsbfjchsdjh")
+    console.log(this.milestoneDetails, 'hjdsbfjchsdjh');
   }
-
 }
--- OKR-Frontend/src/app/objective-details/objective-details.component.ts
+++ OKR-Frontend/src/app/objective-details/objective-details.component.ts
@@ -5,96 +5,107 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-objective-details',
   templateUrl: './objective-details.component.html',
-  styleUrls: ['./objective-details.component.css']
+  styleUrls: ['./objective-details.component.css'],
 })
 export class ObjectiveDetailsComponent implements OnInit {
+  constructor(private http: HttpClient, private route: ActivatedRoute) {}
 
-  constructor(private http : HttpClient, private route : ActivatedRoute) { }
+  Id = {
+    goal_id: '',
+  };
+  org_id: any;
+  goal_data: any;
+  milestone_data: any;
 
-  Id={
-    goal_id:""
-  }
-  org_id:any;
-  goal_data:any;
-  milestone_data:any;
-
-  deleteMilestoneReq={ 
-    milestone_id:"",
-    org_id:"",
-  }
+  deleteMilestoneReq = {
+    milestone_id: '',
+    org_id: '',
+    goal_id: '',
+  };
   headers = {
     'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-    'Accept': 'application/json',
+    Accept: 'application/json',
     'Access-Control-Allow-Headers': '*',
-    'x-access-token' : 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
-    'x-key':'1',
-    'x-org':'1'
+    'x-access-token':
+      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2NDc0OTcyMzA5MTQsImlzcyI6InFpbG8iLCJhdWQiOjF9.Kv9zMVAcDRpCjH3mqxv9tNoFOQoEwJOfOzFWsGyP2hg',
+    'x-key': '1',
+    'x-org': '1',
     // 'x-access-token' : JSON.parse(JSON.stringify(sessionStorage.getItem("token"))),
     // 'x-key':JSON.parse(JSON.stringify(sessionStorage.getItem("user_id"))),
     // 'x-org':JSON.parse(JSON.stringify(sessionStorage.getItem("orgDetails_id")))
-  }  
-  requestOptions = { 
+  };
+  requestOptions = {
     headers: new HttpHeaders(this.headers),
   };
 
-  deleteMilestone=(milestone_name: any, milestone_id:any , org_id: any)=>{
-    
-    this.deleteMilestoneReq.milestone_id= milestone_id;
+  deleteMilestone = (
+    milestone_name: any,
+    milestone_id: any,
+    org_id: any,
+    goal_id: any
+  ) => {
+    this.deleteMilestoneReq.milestone_id = milestone_id;
     this.deleteMilestoneReq.org_id = org_id;
-    console.log(this.deleteMilestoneReq)
-    if(confirm("Are you sure want to delete "+milestone_name)){
-      this.http.put(`/api/v1/employee/deletemilestone`, {milestone_id,org_id} , this.requestOptions).subscribe((response)=>{
-        console.log("delete goal console:---", response);
-        window.location.reload();
-      },(error)=>{
-        console.error(error);
-      })
+    this.deleteMilestoneReq.goal_id = goal_id;
+    console.log('GOal id of milestone:---', this.deleteMilestoneReq.goal_id);
+    console.log(this.deleteMilestoneReq);
+    if (confirm('Are you sure want to delete ' + milestone_name)) {
+      this.http
+        .put(
+          `/api/v1/employee/deletemilestone`,
+          { milestone_id, org_id },
+          this.requestOptions
+        )
+        .subscribe(
+          (response) => {
+            console.log('delete goal console:---', response);
+            window.location.reload();
+          },
+          (error) => {
+            console.error(error);
+          }
+        );
     }
-  }
+  };
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe((params)=>{
-    
-      this.Id.goal_id=params["ID"];
-     
+    this.route.queryParams.subscribe((params) => {
+      this.Id.goal_id = params['ID'];
+
       //  const myorgid = sessionStorage.getItem('orgDetails_id');
       // console.log(myorgid)
-    })    
+    });
     this.getGoalDetails();
     this.getGoalMilestones();
-   
-
   }
 
-  getGoalDetails(){
-  // Get Goals
+  getGoalDetails() {
+    // Get Goals
 
-  
-  return this.http.post(`/api/v1/employee/getgoaldetails`, this.Id, this.requestOptions).subscribe((response)=>{
-   
-    this.goal_data=response;
-    console.log(this.goal_data);
-    
-   
-    
-  },(error)=>{
-    console.error(error);
-  })
-
+    return this.http
+      .post(`/api/v1/employee/getgoaldetails`, this.Id, this.requestOptions)
+      .subscribe(
+        (response) => {
+          this.goal_data = response;
+          console.log(this.goal_data);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   }
 
-  getGoalMilestones(){
-
-  // Get Goals
-  return this.http.post(`/api/v1/employee/getgoalmilestones`, this.Id, this.requestOptions).subscribe((response)=>{
-   
-    this.milestone_data=response;
-  
-    
-  },(error)=>{
-    console.error(error);
-  })
-
+  getGoalMilestones() {
+    // Get Goals
+    return this.http
+      .post(`/api/v1/employee/getgoalmilestones`, this.Id, this.requestOptions)
+      .subscribe(
+        (response) => {
+          this.milestone_data = response;
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   }
-
 }
