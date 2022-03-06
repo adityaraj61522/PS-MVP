48df883ba76333ff64d098d604173fb614dd38fb
AnkitKuamr117 <ankitpandel1234@gmail.com>
2022-03-06 14:14:04 +0530

    .

--- OKR-Frontend/src/app/milestone/toast/toast-global.module.ts
+++ OKR-Frontend/src/app/milestone/toast/toast-global.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { NgbdToastGlobal } from './toast-global.component';
-import { ToastsContainer } from './toasts-container.component';
+// import { NgbdToastGlobal } from './toast-global.component';
+// import { ToastsContainer } from './toasts-container.component';
 
 @NgModule({
   imports: [BrowserModule, NgbModule],
-  declarations: [NgbdToastGlobal, ToastsContainer],
-  bootstrap: [NgbdToastGlobal]
+  // declarations: [NgbdToastGlobal, ToastsContainer],
+  // bootstrap: [NgbdToastGlobal]
 })
 export class NgbdToastGlobalModule {}
