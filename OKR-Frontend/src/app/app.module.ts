eabb5e085f2b9181b14318faaf8a93284ccdc9ff
Aditya Raj <ADITYARAJ61522@GMAIL.COM>
2022-03-07 16:52:54 +0530

    fix

--- OKR-Frontend/src/app/app.module.ts
+++ OKR-Frontend/src/app/app.module.ts
@@ -28,7 +28,6 @@ import { TeamListComponent } from './my-team/team-list/team-list.component';
 import { SettingsComponent } from './admin/settings/settings.component';
 import { UsersComponent } from './admin/users/users.component';
 import { UserCardComponent } from './admin/users/user-card/user-card.component';
-import { EditUsersComponent } from './admin/users/user-card/edit-users/edit-users.component';
 import { CheckinPopupComponent } from './objective-details/milestone-list/checkin-popup/checkin-popup.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MilestoneReuseComponent } from './milestone/milestone-reuse/milestone-reuse.component';
@@ -58,7 +57,6 @@ import { ToastsContainer } from './milestone/toast/toasts-container.component';
     SettingsComponent,
     UsersComponent,
     UserCardComponent,
-    EditUsersComponent,
     CheckinPopupComponent,
     MilestoneReuseComponent,
     NgbdToastGlobal,
